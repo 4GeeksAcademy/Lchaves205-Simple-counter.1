bab5278d45d0b68b8ecd555447c0e483dfa11175
Leo Chaves <leingas13@gmail.com>
2024-11-13 22:39:40 +0000

    final version working

---
+++ src/js/icons.js
@@ -0,0 +1,8 @@
+import { library, config } from '@fortawesome/fontawesome-svg-core';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+
+// Optional: Configure FontAwesome if needed
+config.autoReplaceSvg = 'nest';
+
+// Add icons to the library
+library.add(faClock);
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,26 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
+// Import React into the bundle
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Include bootstrap npm library into the bundle
+import 'bootstrap';
+import './icons.js';
+import SimpleCounter from "./component/simpleCounter.jsx"
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
+// Include your index.scss file into the bundle
+import '../styles/index.scss';
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let counter = 0;
+const root = ReactDOM.createRoot(document.getElementById('app'));
 
+setInterval(() => {
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100);
+    const two = Math.floor(counter / 10);
+    const one = Math.floor(counter / 1);
+    counter++;
+    
+    root.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />
+    );
+}, 1000);
